a2b3b3902deb35b253d7011e3e5484fd9bea3b1a
musabhaqqani <musabhaqqani70@gmail.com>
2024-02-12 22:02:21 +0530

    Final navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,15 +9,14 @@ export default function navbar() {
         <div>
             <div id="circularMenu" className="circular-menu">
 
-                <a className="floating-btn" onClick={menuPop}>
+                <i className="floating-btn" onClick={menuPop}>
                     <i className="fa fa-plus"></i>
-                </a>
-
+                </i>
                 <menu className="items-wrapper">
-                    <a href="#about&home" className="menu-item fa fa-home"></a>
-                    <a href="#resume" className="menu-item fa fa-user"></a>
-                    <a href="#" class="menu-item fa fa-code"></a>
-                    <a href="#" className="menu-item fa fa-envelope"></a>
+                    <i  className="menu-item fa fa-home"></i>
+                    <i  className="menu-item fa fa-user"></i>
+                    <i  className="menu-item fa fa-code"></i>
+                    <i  className="menu-item fa fa-envelope"></i>
                 </menu>
 
             </div>
